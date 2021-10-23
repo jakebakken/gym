99bee8347c1fd6a7a8508806e406e82707503a84
Jake <jakebakken1@gmail.com>
2021-10-23 16:16:29 -0700

    added connect to db try into app.py

--- app.py
+++ app.py
@@ -25,10 +25,33 @@ server = app.server
 debug = False
 
 DATABASE_URL = os.environ['DATABASE_URL']
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-cur = conn.cursor()
+conn = None
+try:
+    # create a new database connection by calling the connect() function
+    conn = psycopg2.connect(DATABASE_URL)
 
-cur.execute('\d')
+    #  create a new cursor
+    cur = conn.cursor()
+
+    # execute an SQL statement to get the HerokuPostgres database version
+    # print('PostgreSQL database version:')
+    cur.execute('SELECT version()')
+
+    # display the PostgreSQL database server version
+    db_version = cur.fetchone()
+    # print(db_version)
+
+    # close the communication with the HerokuPostgres
+    cur.close()
+except Exception as error:
+    print('Cause: {}'.format(error))
+
+finally:
+    # close the communication with the database server by calling the close()
+    if conn is not None:
+        conn.close()
+        print('Database connection closed.')
+
 
 # cardio & exercise datatable columns
 cardio_cols = [
