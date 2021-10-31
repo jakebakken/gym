bd391148fa523631c982cd3ad50174039bab3f46
Jake <jakebakken1@gmail.com>
2021-10-31 16:49:44 -0700

    removed attempt to require login from views.py

--- flask_dir/views.py
+++ flask_dir/views.py
@@ -18,10 +18,6 @@ def exercise_page():
     return render_template('exercise.html')
 
 
-@views.route('/exercise_dashboard/')
-@login_required
-
-
 @views.route('/signup')
 def signup_page():
     return render_template('signup.html')
