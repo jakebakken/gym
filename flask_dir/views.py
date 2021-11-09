c4042c78fc59ba5f88247821b45ef236faa781a2
Jake <jakebakken1@gmail.com>
2021-11-08 23:56:09 -0800

    split start and end workout-button's, see how data gets written to db

--- flask_dir/views.py
+++ flask_dir/views.py
@@ -21,21 +21,28 @@ def exercise_page():
     if request.method == 'POST':
         if 'start-workout-button' in request.form:
             workout_date = dt.datetime.now().date()
-            workout_name = request.form.get('workout-name')
             workout_start_time = dt.datetime.now()
+
+            start_of_workout = Workout(
+                user_id=current_user.id, workout_date=workout_date,
+                workout_start_time=workout_start_time,
+            )
+            db.session.add(start_of_workout)
+            db.session.commit()
+
+            flash("Workout Started", category='success')
+
+        elif 'end-workout-button' in request.form:
+            workout_name = request.form.get('workout-name')
             workout_end_time = dt.datetime.now()
             rating = request.form.get('workout-rating')
 
-            new_workout = Workout(
-                user_id=current_user.id, workout_date=workout_date,
-                workout_name=workout_name, workout_start_time=workout_start_time,
-                workout_end_time=workout_end_time, rating=rating,
+            end_of_workout = Workout(
+                workout_name=workout_name, workout_end_time=workout_end_time,
+                rating=rating,
             )
-            db.session.add(new_workout)
+            db.session.add(end_of_workout)
             db.session.commit()
-
-            flash("Workout Saved", category='success')
-            return '', 200
 
     return render_template('exercise.html', user=current_user)
 
