62ee93937b7c6bff191734cd2306c78344c230ee
Jake <jakebakken1@gmail.com>
2021-11-08 23:05:51 -0800

    checkout last commit before gym, some db values were added
    didn't realize but right before leaving for the gym, 7 db entries were
    recorded, without workout names, in a span of 10 min... seeing if this
    was the one

--- flask_dir/views.py
+++ flask_dir/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask import Blueprint, render_template, request, flash
 from flask_login import login_required, current_user
 from .models import Workout, Exercise, Set
 from . import db
@@ -18,23 +18,28 @@ def home_page():
 @views.route('/exercise/', methods=['GET', 'POST'])
 @login_required
 def exercise_page():
+    return render_template('exercise.html', user=current_user)
+
+
+@views.route('/start-workout/', methods=['POST'])
+@login_required
+def submit_start_workout_data():
     if request.method == 'POST':
-        if request.form['start-workout-button']:
-            workout_date = dt.datetime.now().date()
-            workout_name = request.form.get('workout-name')
-            workout_start_time = dt.datetime.now()
-            workout_end_time = dt.datetime.now()
-            rating = request.form.get('workout-rating')
+        workout_date = dt.datetime.now().date()
+        workout_name = request.form.get('workout-name')
+        workout_start_time = dt.datetime.now()
+        workout_end_time = dt.datetime.now()
+        rating = request.form.get('workout-rating')
 
-            new_workout = Workout(
-                user_id=current_user.id, workout_date=workout_date,
-                workout_name=workout_name, workout_start_time=workout_start_time,
-                workout_end_time=workout_end_time, rating=rating,
-            )
-            db.session.add(new_workout)
-            db.session.commit()
+        new_workout = Workout(
+            user_id=current_user.id, workout_date=workout_date,
+            workout_name=workout_name, workout_start_time=workout_start_time,
+            workout_end_time=workout_end_time, rating=rating,
+        )
+        db.session.add(new_workout)
+        db.session.commit()
 
-            return flash("Workout Saved", category='success')
+        return flash("Workout Saved", category='success')
 
     return render_template('exercise.html', user=current_user)
 
