b489945185ac3b92612f633ba2d6ccf07cbf6a2c
Jake <jakebakken1@gmail.com>
2021-11-11 22:40:35 -0800

    reverting to push where views.route() has separate func

--- flask_dir/views.py
+++ flask_dir/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_required, current_user
 from .models import Workout, Exercise, Set
 from . import db
@@ -28,34 +28,39 @@ def exercise_page():
                 workout_start_time=workout_start_time,
             )
             db.session.add(start_of_workout)
+            db.session.commit()
 
             flash("Workout Started", category='success')
 
-        elif 'finish-workout-button' in request.form:
-            workout_name = request.form.get('workout-name')
-            workout_end_time = dt.datetime.now()
-            rating = request.form.get('workout-rating')
-
-            if not workout_name:
-                flash("Workout needs a name to be saved", category='error')
-
-            else:
-                # get the latest created workout from current user
-                #  (aka the incomplete entry of this current workout)
-                current_workout = Workout.query.filter_by(user_id=current_user.id).order_by(Workout.id.desc()).first()
-
-                # fill in missing values
-                current_workout.workout_name = workout_name
-                current_workout.workout_end_time = workout_end_time
-                current_workout.rating = rating
-                db.session.commit()
-
-                flash("Workout Finished", category='success')
-
-        # todo page cannot refresh every submission, because a whole workout
-        #  would be erased just because a user tried to submit with no name lol
-
     return render_template('exercise.html', user=current_user)
+
+
+@views.route('/finish')
+@login_required
+def finish():
+    if request.method == 'POST':
+        workout_name = request.form.get('workout-name')
+        workout_end_time = dt.datetime.now()
+        rating = request.form.get('workout-rating')
+
+        if not workout_name:
+            flash("Workout needs a name to be saved", category='error')
+            return ''
+
+        else:
+            # get the latest created workout from current user
+            #  (aka the incomplete entry of this current workout)
+            current_workout = Workout.query.filter_by(
+                user_id=current_user.id).order_by(Workout.id.desc()).first()
+
+            # fill in missing values
+            current_workout.workout_name = workout_name
+            current_workout.workout_end_time = workout_end_time
+            current_workout.rating = rating
+            db.session.commit()
+
+            flash("Workout Finished", category='success')
+            return workout_name  # value return back to exercise name input
 
 
 @views.route('/signup')
