3367f1495f2f159ba851dc1273839af05a1c49a1
Jake <jakebakken1@gmail.com>
2021-11-12 19:54:12 -0800

    cleanup push
    buttons have separate route() funcs
    start workout button has own ajax + views.route() + hide after ajax
    complete
    properly referenced addSet + addExercise funcs from respective buttons

--- flask_dir/views.py
+++ flask_dir/views.py
@@ -18,49 +18,54 @@ def home_page():
 @views.route('/exercise/', methods=['GET', 'POST'])
 @login_required
 def exercise_page():
-    if 'start-workout-button' in request.form:
-        workout_date = dt.datetime.now().date()
-        workout_start_time = dt.datetime.now()
-
-        start_of_workout = Workout(
-            user_id=current_user.id, workout_date=workout_date,
-            workout_start_time=workout_start_time,
-        )
-        db.session.add(start_of_workout)
-        db.session.commit()
-
-        flash("Workout Started", category='success')
-
     return render_template('exercise.html', user=current_user)
 
 
-@views.route('/finish', methods=['POST'])
+@views.route('/start-workout', methods=['POST'])
 @login_required
-def finish():
-    if 'finish-workout-button' in request.form:
-        workout_name = request.form['workoutName']
-        workout_end_time = dt.datetime.now()
-        rating = request.form['rating']
+def start_workout():
+    workout_date = dt.datetime.now().date()
+    workout_start_time = dt.datetime.now()
 
-        # get the latest created workout from current user
-        #  (aka the incomplete entry of this current workout)
-        current_workout = Workout.query.filter_by(
-            user_id=current_user.id).order_by(Workout.id.desc()).first()
+    start_of_workout = Workout(
+        user_id=current_user.id, workout_date=workout_date,
+        workout_start_time=workout_start_time,
+    )
+    db.session.add(start_of_workout)
+    db.session.commit()
 
-        # fill in missing values
-        current_workout.workout_name = workout_name
-        current_workout.workout_end_time = workout_end_time
-        current_workout.rating = rating
-        db.session.commit()
+    flash("Workout Started", category='success')
+    return jsonify({
+        'result': 'success',
+    })
 
-        flash("Workout Finished", category='success')
 
-        # value return back to exercise name input
-        return jsonify({
-            'result': 'success',
-            'workout_name': workout_name,
-            'rating': rating,
-        })
+@views.route('/finish-workout', methods=['POST'])
+@login_required
+def finish_workout():
+    workout_name = request.form['workoutName']
+    workout_end_time = dt.datetime.now()
+    rating = request.form['rating']
+
+    # get the latest created workout from current user
+    #  (aka the incomplete entry of this current workout)
+    current_workout = Workout.query.filter_by(
+        user_id=current_user.id).order_by(Workout.id.desc()).first()
+
+    # fill in missing values
+    current_workout.workout_name = workout_name
+    current_workout.workout_end_time = workout_end_time
+    current_workout.rating = rating
+    db.session.commit()
+
+    flash("Workout Finished", category='success')
+
+    # value return back to exercise name input
+    return jsonify({
+        'result': 'success',
+        'workout_name': workout_name,
+        'rating': rating,
+    })
 
 
 @views.route('/signup')
