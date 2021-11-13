9c5c93590a48c6bd79db41e15a1444afc0790d11
Jake <jakebakken1@gmail.com>
2021-11-12 18:44:51 -0800

    exercise_page() only uses GET
    last push worked for committing workout name to DB !
    exercise_page() won't trigger render_template now when ajax posts on
    finish workout click - to see if value populates correctly to input
    field

--- flask_dir/views.py
+++ flask_dir/views.py
@@ -15,22 +15,22 @@ def home_page():
     return render_template('home.html', user=current_user)
 
 
-@views.route('/exercise/', methods=['GET', 'POST'])
+@views.route('/exercise/', methods=['GET'])
 @login_required
 def exercise_page():
-    if request.method == 'POST':
-        if 'start-workout-button' in request.form:
-            workout_date = dt.datetime.now().date()
-            workout_start_time = dt.datetime.now()
-
-            start_of_workout = Workout(
-                user_id=current_user.id, workout_date=workout_date,
-                workout_start_time=workout_start_time,
-            )
-            db.session.add(start_of_workout)
-            db.session.commit()
-
-            flash("Workout Started", category='success')
+    # if request.method == 'POST':
+    #     if 'start-workout-button' in request.form:
+    #         workout_date = dt.datetime.now().date()
+    #         workout_start_time = dt.datetime.now()
+#
+    #         start_of_workout = Workout(
+    #             user_id=current_user.id, workout_date=workout_date,
+    #             workout_start_time=workout_start_time,
+    #         )
+    #         db.session.add(start_of_workout)
+    #         db.session.commit()
+#
+    #         flash("Workout Started", category='success')
 
     return render_template('exercise.html', user=current_user)
 
