d0e83fbb6f4d3b1a0ff9043a5720346516a2fae2
Jake <jakebakken1@gmail.com>
2021-11-15 20:19:18 -0800

    try to return workout names to collapse buttons
    last push works and is responsive to db changes!
    gotta figure out how to get the values now

--- flask_dir/views.py
+++ flask_dir/views.py
@@ -14,8 +14,9 @@ views = Blueprint('views', __name__)
 def home_page():
     workouts = Workout.query.filter_by(
         user_id=current_user.id).order_by(Workout.id.desc()).all()
+    names = [workout.workout_name for workout in workouts]
 
-    return render_template('home.html', user=current_user, workouts=workouts)
+    return render_template('home.html', user=current_user, workouts=workouts, names=names)
 
 
 @views.route('/exercise/', methods=['GET', 'POST'])
