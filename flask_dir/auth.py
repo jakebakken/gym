817cd19675729f68dd98087747bf8fc64f050b3a
Jake <jakebakken1@gmail.com>
2021-11-21 21:59:32 -0800

    signup verfication with js

--- flask_dir/auth.py
+++ flask_dir/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, flash, redirect, url_for, render_template
+from flask import Blueprint, request, flash, redirect, url_for, render_template, jsonify
 from flask_login import login_user, logout_user, login_required, current_user
 import os
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -41,81 +41,37 @@ def logout():
     return redirect(url_for('auth.login'))
 
 
+@auth.route('/signup_user', methods=['POST'])
+def sign_up_new_user():
+    first_name = request.form['firstVal']
+    last_name = request.form['lastVal']
+    username = request.form['usernameVal']
+    email = request.form['emailVal']
+    password = request.form['passwordVal']
+
+    # query for existing user by entered email
+    user = Users.query.filter_by(email=email).first()
+
+    # if user doesn't exist in database, add new user
+    if not user:
+        new_user = Users(
+            first_name=first_name, last_name=last_name, username=username,
+            email=email, password=generate_password_hash(
+                password, method='sha256'
+            )
+        )
+        # add new user to db
+        db.session.add(new_user)
+        db.session.commit()
+        login_user(new_user, remember=True)
+        flash("Account Created", category='success')
+        return redirect(url_for('views.home_page'))
+    else:
+        flash("An account with this email already exists", category='error')
+
+    return jsonify({'result': 'success'})
+
+
 @auth.route('/signup', methods=['GET', 'POST'])
 def sign_up():
-    special_chars = [
-        '!', '”', '#', '$', '%', '&', '’', '(', ')', '*', '+', ',', '-', '.',
-        '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`',
-        '{', '|', '}', '~'
-    ]
-
-    if request.method == 'POST':
-        first_name = request.form.get('first_name')
-        last_name = request.form.get('last_name')
-        username = request.form.get('username')
-        email = request.form.get('email')
-        password = request.form.get('password')
-        password_confirm = request.form.get('password_confirm')
-
-        if len(first_name) < 1:
-            flash("First name is required", category='error')
-        if len(first_name) > 50:
-            flash("First name can't be over 50 characters", category='error')
-        if len(last_name) < 1:
-            flash("Last name is required", category='error')
-        if len(last_name) > 50:
-            flash("Last name can't be over 50 characters", category='error')
-        if len(username) < 1:
-            flash("Username is required", category='error')
-        if len(username) > 50:
-            flash("Username can't be over 50 characters", category='error')
-
-        if len(email) < 5:
-            flash("Email is required", category='error')
-        if len(email) > 100:
-            flash("Last name can't be over 100 characters", category='error')
-
-        if password != password_confirm:
-            flash("Passwords do not match", category='error')
-        if len(password) < 8:
-            flash("Password must be at least 8 characters", category='error')
-        if len(password) > 50:
-            flash("Password can't be over 50 characters", category='error')
-        if not any(char.isupper() for char in password):
-            flash("Password must contain at least 1 uppercase character", category='error')
-        if not any(char.islower() for char in password):
-            flash("Password must contain at least 1 lowercase character", category='error')
-        if not any(char.isnumeric() for char in password):
-            flash("Password must contain at least 1 number", category='error')
-        if not any(char in special_chars for char in password):
-            flash("Password must contain at least one special character", category='error')
-
-        if 50 >= len(first_name) >= 1 and 50 >= len(last_name) >= 1 and \
-                50 >= len(username) >= 1 and 100 >= len(email) >= 5 and \
-                password == password_confirm and 50 >= len(password) >= 8 and \
-                any(char.isupper() for char in password) and \
-                any(char.islower() for char in password) and \
-                any(char.isnumeric() for char in password) and \
-                any(char in special_chars for char in password):
-
-            # query for existing user by entered email
-            user = Users.query.filter_by(email=email).first()
-
-            # if user doesn't exist in database, add new user
-            if not user:
-                new_user = Users(
-                    first_name=first_name, last_name=last_name, username=username,
-                    email=email, password=generate_password_hash(
-                        password, method='sha256'
-                    )
-                )
-                # add new user to db
-                db.session.add(new_user)
-                db.session.commit()
-                login_user(new_user, remember=True)
-                flash("Account Created", category='success')
-                return redirect(url_for('views.home_page'))
-            else:
-                flash("An account with this email already exists", category='error')
-
     return render_template('signup.html', user=current_user)
---
+++ flask_dir/static/signup.js
@@ -0,0 +1,87 @@
+const signupButton = document.getElementById("signup-button");
+const firstName = document.getElementById("first_name");
+const lastName = document.getElementById("last_name");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const passwordConfirm = document.getElementById("password_confirm");
+
+
+signupButton.onclick = function checkInfo() {
+    firstValid = false;
+    lastValid = false;
+    usernameValid = false;
+    emailValid = false;
+    passwordValid = false;
+    passwordConfirmValid = false;
+    passwordMatch = false;
+
+    firstVal = firstName.value;
+    lastVal = lastName.value;
+    usernameVal = username.value;
+    emailVal = email.value;
+    passwordVal = password.value;
+    passwordConfirmVal = passwordConfirm.value;
+
+    if (firstVal.length > 1 && firstVal.length <= 50) {
+        firstValid = true;
+    } else {
+        let firstMessage = "First name has to be between 1 - 50 letters"
+    }
+
+    if (lastVal.length > 1 && lastVal.length <= 50) {
+        lastValid = true;
+    } else {
+        let lastMessage = "Last name has to be between 1 - 50 letters"
+    }
+
+    if (usernameVal.length > 1 && usernameVal.length <= 50) {
+        usernameValid = true;
+    } else {
+        let usernameMessage = "Username has to be between 1 - 50 letters"
+    }
+
+    if (emailVal.length > 5) {
+        emailValid = true;
+    } else {
+        let emailMessage = "Email is too short"
+    }
+
+    if (passwordVal.length > 8) {
+        passwordValid = true;
+    } else {
+        let passwordMessage = "Password is too short"
+    }
+
+    if (passwordConfirmVal.length > 8) {
+        passwordConfirmValid = true;
+    } else {
+        let passwordConfirmMessage = "Password is too short"
+    }
+
+    if (passwordVal === passwordConfirmVal) {
+        passwordMatch = true;
+    } else {
+        let passwordMatchMessage = "Passwords must match"
+    }
+
+    if (firstValid==true && lastValid==true && usernameValid==true &&
+    emailValid==true && passwordValid==true && passwordConfirmValid==true
+    && passwordMatch==true) {
+        signup = $.ajax({
+            type: 'POST',
+            url: '/signup_user',
+            data: {
+                firstVal: firstVal,
+                lastVal: lastVal,
+                usernameVal: usernameVal,
+                emailVal: emailVal,
+                passwordVal: passwordVal,
+            },
+            success: function() {
+                console.log("user signed up")
+            }
+        })
+    }
+    // for each false input value, show respective p tag tip
+};
